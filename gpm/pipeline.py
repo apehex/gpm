d89153d5815c1c353eb89ba014f910685cc1d63d
apehex <17317183+apehex@users.noreply.github.com>
2024-05-05 16:19:38 +0200

    Remove external deps

---
+++ gpm/pipeline.py
@@ -0,0 +1,24 @@
+# MAPPINGS ####################################################################
+
+def mappings(voc: list) -> dict:
+    __itos = {__i: __c for __i, __c in enumerate(voc)}
+    __stoi = {__c: __i for __i, __c in enumerate(voc)}
+    # blank placeholder
+    __blank_c = __itos[0] # chr(0)
+    __blank_i = 0
+    # s => i
+    def __encode(c: str) -> int:
+        return __stoi.get(c, __blank_i)
+    # i => s
+    def __decode(i: int) -> str:
+        return __itos.get(i, __blank_c)
+    # return both
+    return {'encode': __encode, 'decode': __decode}
+
+# ENCODING ####################################################################
+
+def encode(text: str, stoi: callable) -> list:
+    return [stoi(__c) for __c in text] # defaults to 0 if a character is not in the vocabulary
+
+def decode(sequence: list, itos: callable) -> list:
+    return ''.join([itos(__i) for __i in sequence]) # defaults to the first character
